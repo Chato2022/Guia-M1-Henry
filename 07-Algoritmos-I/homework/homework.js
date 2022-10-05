dcf5eb7060eb6bc6ae37c6a95a089700cb76ee5f
Julio <julio.wow.1997@gmail.com>
2022-10-05 13:44:19 -0500

    complete homework-07-Algoritmos_I

--- 07-Algoritmos-I/homework/homework.js
+++ 07-Algoritmos-I/homework/homework.js
@@ -6,7 +6,20 @@ function factorear(num) {
   // los factores por los cuales se va dividiendo a dicho número (De menor a mayor)
   // Ej: factorear(180) --> [1, 2, 2, 3, 3, 5] Ya que 1x2x2x3x3x5 = 180 y son todos números primos
   // Tu código:
-
+  var Array=[];
+  var i=1;
+  do {
+    if(Number.isInteger(num/i)){
+      num=num/i;
+      Array.push(i);
+      if(num%i===0&&i!==1){
+        i=i;
+      }else{
+        i++;
+      }
+    }else{i++}
+  } while (num!==1)
+  return Array;
 }
 
 function bubbleSort(array) {
@@ -14,7 +27,38 @@ function bubbleSort(array) {
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
   // Tu código:
-
+/*  var comparacion=[]
+  array.forEach(element => {
+    comparacion.push(element);
+  });
+  
+  for(var i=0;i<(array.length-1);i++){
+    var a=array[i];
+    var b=array[i+1];
+    var temp=0;
+    if(a===b){
+      a=b;
+    }
+    else if(a>b){
+      temp=b;
+      b=a;
+      a=temp;
+    }
+  }
+  if(comparacion.sort((a,b)=>a-b)!==array){
+    bubbleSort(array)
+  }
+  return array*/
+  for (let i = 0; i< (array.length-1); i++) {
+    for (let j = 0; j< (array.length-1); j++) {
+      if (array[j]>array[j+1]) {
+        var aux=array[j+1];
+        array[j+1]=array[j];
+        array[j]=aux;
+      }
+    }
+  }
+  return array;
 }
 
 
@@ -23,7 +67,16 @@ function insertionSort(array) {
   // el array recibido como parámetro utilizando arreglos
   // Devolver el array ordenado resultante
   // Tu código:
-
+  for (let i = 1; i < array.length; i++) {
+    let j=i-1;
+    var aux=array[i];
+    while (j>=0&&array[j]> aux){
+      array[j+1]=array[j];
+      j--;
+    }
+    array[j+1]=aux;
+  }
+  return array;
 }
 
 
@@ -32,7 +85,31 @@ function selectionSort(array) {
   // el array recibido como parámetro utilizando dos arreglos
   // Devolver el array ordenado resultante
   // Tu código:
-
+  /*let aux=[];
+  array.forEach(element => {
+    aux.push(element);
+  });
+  for (let i = 0; i < array.length; i++) {
+    let min= aux.slice(i+1).reduce((a,e,j) => (e< aux[a] ? j+i+1 : a),i);
+    if(min!=i){
+      [aux[i], aux[min]]=[aux[min], aux[i]];
+    }
+  }
+  return aux;*/
+  for (let i = 0; i < array.length; i++) {
+    var min=i;
+    for (let j = i+1; j < array.length; j++) {
+      if(array[j]<array[min]){
+        min=j;
+      }
+    }
+    if (min!==i) {
+      var aux= array[i];
+      array[i]=array[min];
+      array[min]=aux;
+    }
+  }
+  return array;
 }
 
 
